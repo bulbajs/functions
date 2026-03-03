f27f1d1c96b5d7be8284d6edb0cd59194d43bb8c
bulbajs <artval.kuptsov@gmail.com>
2026-03-03 18:01:58 +0300

    local and global variables
    variable analysis

--- functions_day1.py
+++ functions_day1.py
@@ -221,3 +221,14 @@ print_perm_time_call(msc_time)
 #     print(s)
 #     s = (':').join(s)
 # print(s)
+
+# def print_symbol_counts(s):
+#     pass
+
+birds = 5000    # глобальная переменная
+
+def print_texas():
+    print('В Техасе обитает', birds, 'птиц.')
+
+def print_california():
+    print('В Калифорнии обитает', birds, 'птиц.')
