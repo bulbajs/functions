853f644a4187a905810386ce2159361bd641b728
bulbajs <artval.kuptsov@gmail.com>
2026-07-14 07:45:00 +0300

    again begin

--- functions_day1.py
+++ functions_day1.py
@@ -995,32 +995,38 @@ prembank1 = PremiumAccount('Petr', 500)
 #===== Task 10 =====
 # Game Character + Weapon Сделай: Weapon: damage
 # Character:name self.weapon = Weapon(...) метод attack() использует урон оружия
-class Weapon():
-    def __init__(self, damage = 100):
-        self.damage = damage
+# class Weapon():
+#     def __init__(self, damage = 100):
+#         self.damage = damage
+#
+#
+# class Character():
+#     def __init__(self, name):
+#         self.name = name
+#         self.weapon = Weapon()
+#
+#     def attack(self):
+#         print(f'Атака {self.name} составляет {self.weapon.damage} урона')
+#
+#     def info(self):
+#         print(f'Вы выбрали персонажа {self.name}')
+#
+# print('---ДОБРО ПОЖАЛОВАТЬ В ИГРУ---')
+# print('---ГЕНДАЛЬФ НА КОНЕ---')
+# char1 = Character('Gendalf')
+# char1.info()
+# char1.attack()
+#
+#
+# list = [1,2,4,5]
+# count = 0
+# for i in list:
+#     print(type(i))
+#     count= i + count
+# print(count)
+
+def calc(a,b):
+    print(a+b)
 
 
-class Character():
-    def __init__(self, name):
-        self.name = name
-        self.weapon = Weapon()
-
-    def attack(self):
-        print(f'Атака {self.name} составляет {self.weapon.damage} урона')
-
-    def info(self):
-        print(f'Вы выбрали персонажа {self.name}')
-
-print('---ДОБРО ПОЖАЛОВАТЬ В ИГРУ---')
-print('---ГЕНДАЛЬФ НА КОНЕ---')
-char1 = Character('Gendalf')
-char1.info()
-char1.attack()
-
-
-list = [1,2,4,5]
-count = 0
-for i in list:
-    print(type(i))
-    count= i + count
-print(count)
+calc(2,2)
